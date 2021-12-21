70b9174b7a1d7dfab5b938a7734f55d0f18ee697
jordiascension <jordiascension@enterpriseconsulting.es>
2021-12-21 17:54:18 +0100

    Single responsability principle implemented closes #1

---
+++ PrincipiosSolid/SingleResponsability/Correcto/Coche.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrincipiosSolid.SingleResponsability.Correcto
+{
+    class Coche
+    {
+        String marca;
+
+        Coche(String marca) { this.marca = marca; }
+
+        String getMarcaCoche() { return marca; }
+    }
+}
